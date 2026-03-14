4ee99788e7b0f2a8e10d388ccab9a731b8c454ab
youstina <youstinaadel538@gmail.com>
2026-03-15 00:15:41 +0200

    feat: enhance professor availability page with search functionality and improved layout

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,15 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: "/staff/availability/page",
+        destination: "/staff/availability",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
